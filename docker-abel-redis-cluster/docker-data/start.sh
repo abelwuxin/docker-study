029a4414b8ad03eb36ec5521a93e78751d2d8457
Abel Deng <abeldeng@qq.com>
2016-05-09 00:07:47 +0800

    update

--- docker-abel-redis-cluster/docker-data/start.sh
+++ docker-abel-redis-cluster/docker-data/start.sh
@@ -9,5 +9,5 @@
 
 sleep 3
 
-IP=`mob.abeldeng.com`
+IP="crsoft.f3322.net"
 echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
