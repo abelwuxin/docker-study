ae97a3e1ac57bd7da6b74687da4db6c65ce6457d
Abel Deng <abeldeng@qq.com>
2016-05-08 23:35:15 +0800

    update

--- docker-abel-redis-cluster/docker-data/start.sh
+++ docker-abel-redis-cluster/docker-data/start.sh
@@ -1,11 +1,11 @@
-/redis/src/redis-server /redis-conf/7000/redis.conf
-/redis/src/redis-server /redis-conf/7001/redis.conf
-/redis/src/redis-server /redis-conf/7002/redis.conf
-/redis/src/redis-server /redis-conf/7003/redis.conf
-/redis/src/redis-server /redis-conf/7004/redis.conf
-/redis/src/redis-server /redis-conf/7005/redis.conf
-/redis/src/redis-server /redis-conf/7006/redis.conf
-/redis/src/redis-server /redis-conf/7007/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7000/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7001/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7002/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7003/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7004/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7005/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7006/redis.conf
+/redis/src/redis-server --protected-mode no /redis-conf/7007/redis.conf
 
 sleep 3
 
